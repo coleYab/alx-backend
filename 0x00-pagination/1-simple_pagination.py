7cbadae41f8a3117617164bf3573cb27a7c093f5
coleYab <yabume123@gamil.com>
2024-09-01 17:03:03 +0300

    + added get page functionality

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,56 @@
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns a page of baby names
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        index_rng = index_range(page, page_size)
+        data_set = self.dataset()
+        page_data = []
+        
+        if index_rng[0] <= len(data_set) - 1:
+            if index_rng[1] > len(data_set) - 1:
+                return data_set[index_rng[0]:]
+            page_data = data_set[index_rng[0]:index_rng[1]]
+
+        return page_data
+
+
+def index_range(page: int, page_size: int) -> Tuple[int]:
+    """function to count the range of the items to be returned
+
+    Args:
+        page (int): the page count
+        page_size (int): the page size
+
+    Raises:
+        Exception: index is invalid 
+
+    Returns:
+        Tuple[int]: _description_
+    """
+    assert page_size > 0 and page > 0
+    return ((page - 1) * page_size, page * page_size)
