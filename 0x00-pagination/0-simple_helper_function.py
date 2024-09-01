b625f9d86402edea2e42a57590be4d8c90158754
coleYab <yabume123@gamil.com>
2024-09-01 16:37:16 +0300

    + added docstring

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
-# initial code for python3 in my world
+"""
+A Module to hold the helper function
+"""
 
 import typing
 
 def index_range(page: int, page_size: int) -> typing.Tuple[int]:
+    """function to count the range of the items to be returned
+
+    Args:
+        page (int): the page count
+        page_size (int): the page size
+
+    Raises:
+        Exception: index is invalid 
+
+    Returns:
+        typing.Tuple[int]: _description_
+    """
     if page <= 0:
         raise Exception("Invalid index")
     return ((page - 1) * page_size, page * page_size)
