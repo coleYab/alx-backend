4d54fea67ce31592ef331b232597a152ed863cf1
coleYab <yabume123@gamil.com>
2024-09-04 15:11:31 +0300

    add: created mru cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Base cache implementation
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    a class to represent basic caching features
+    """
+    def __init__(self):
+        """ constructor """
+        super().__init__()
+        self.lru_items_list = []
+
+    def put(self, key, item):
+        """ adding a cache to chached data
+        """
+        if None not in [key, item]:
+            if key in self.lru_items_list:
+                self.lru_items_list.remove(key)
+            if len(self.lru_items_list) == BaseCaching.MAX_ITEMS:
+                discarded = self.lru_items_list.pop(0)
+                del self.cache_data[discarded]
+                print("DISCARD: {}".format(discarded))
+
+            self.lru_items_list.insert(0, key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ retriving a cache with the key
+        """
+        if key in self.cache_data:
+            self.lru_items_list.remove(key)
+            self.lru_items_list.insert(0, key)
+            return self.cache_data[key]
