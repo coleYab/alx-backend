f870c27e9cadb5b7e3c36456461d792162fd1136
coleYab <yabume123@gamil.com>
2024-09-04 14:41:21 +0300

    added new item

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -12,22 +12,25 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """ constructor """
         super().__init__()
-        self.__usage_rate = {}
+        self.lru_items_list = []
 
     def put(self, key, item):
         """ adding a cache to chached data
         """
         if None not in [key, item]:
+            if key in self.lru_items_list:
+                self.lru_items_list.remove(key)
+                assert len(self.lru_items_list) < self.MAX_ITEMS && "Unreachable"
+            if len(self.lru_items_list) != 4:
+                discarded = self.lru_items_list.pop(self.MAX_ITEMS - 1)
+                del self.cache_data[discarded]
+                print("DISCARD: {}".format(discarded))
+
+            self.lru_items_list.insert(0, key)
             self.cache_data[key] = item
-            self.__usage_rate[key] = 1
-            if len(self.cache_data.items()) > self.MAX_ITEMS:
-                discard = list(self.cache_data.keys())[-2]
-                del self.cache_data[discard]
-                print("DISCARD {}".format(discard))
 
     def get(self, key):
         """ retriving a cache with the key
         """
         if key in self.cache_data:
-            self.__usage_rate[key] += 1
             return self.cache_data[key]
