e05ba3c918a5c54098d09d188616a82d7fe6ea1d
coleYab <yabume123@gamil.com>
2024-09-13 11:22:12 +0300

    feat: fixed python dependency

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+simple babel configuration
+"""
+from flask import Flask
+from flask import render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    a simple config class for bable
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE ="en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def homepage() -> str:
+    """
+    simple template website
+    """
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+        app.run(port="5000", host="0.0.0.0", debug=True)
